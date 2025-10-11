de3a300fe35676c9b84a9ab456f1fad95afe71f3
Sidhi Raj <siddhir954@gmail.com>
2025-10-11 16:34:37 +0530

    Fetched and ADD categories method

--- src/main/java/com/ecommerce/sb_ecom/controller/CategoryController.java
+++ src/main/java/com/ecommerce/sb_ecom/controller/CategoryController.java
@@ -1,4 +1,20 @@
 package com.ecommerce.sb_ecom.controller;
 
+import com.ecommerce.sb_ecom.model.Category;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CategoryController {
+    private List<Category> categoryList = new ArrayList<>();
+
+    public List<Category> getCategoryList() {
+        return categoryList;
+    }
+
+    public String createCategory(@RequestBody Category category) {
+        categoryList.add(category);
+        return "successfully added Category";
+    }
 }
